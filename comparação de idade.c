db4ce92a0130d164450ae1a25aa471f879e0ac37
Ray.exe✭ <raylasilva119@gmail.com>
2024-10-08 09:38:58 -0300

    Add files via upload

---
+++ comparação de idade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+   int idadeBruno= 4;
+   int idadeCeceu= 2;
+
+    if(idadeBruno > idadeCeceu){
+        printf("Bruno e mais velho que Ceceu");
+    }
+    else printf("Ceceu e mais velha que Bruno");
+ }
